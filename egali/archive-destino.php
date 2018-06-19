9769297e92a7a4055020325b6b8a62959cbbef17
Giovane Zignani <giovane.zignani@conectt.com.br>
2018-06-19 16:02:28 -0300

    estrutura tradução archive destino

--- egali/archive-destino.php
+++ egali/archive-destino.php
@@ -10,8 +10,8 @@ get_header();
 				<div class="row">
 					<nav aria-label="breadcrumb">
 						<ol class="breadcrumb">
-							<li class="breadcrumb-item"><a href="<?php echo get_site_url();?>">Home</a></li>
-							<li class="breadcrumb-item">Destinos</li>
+							<li class="breadcrumb-item"><a href="<?php echo get_site_url();?>"><?php _e('Home','egali'); ?></a></li>
+							<li class="breadcrumb-item"><?php _e('Destinos','egali'); ?></li>
 						</ol>
 					</nav>
 				</div>
@@ -22,7 +22,7 @@ get_header();
 			<div class="container">
 				<div class="row mb-5">
 					<div class="col-12">
-						<h1>DESTINOS</h1>
+						<h1><?php _e('DESTINOS','egali'); ?></h1>
 						<div class="line-h-6 mt-4"></div>
 					</div>
 				</div>
@@ -48,7 +48,7 @@ get_header();
 										<div class="card-img-top" style="background-image:url('<?php echo $imgUrl ?>');height:180px;background-size:cover;"></div>                                
 										<div class="card-body" style="box-shadow: 0px 20px 40px rgba(0, 0, 0, 0.1);">
 											<h5 class="card-title"><?php echo $pais["nome"]; ?></h5>
-											<a class="btn-secondary" style="display:block" href="<?php echo $pais["link"];?>">Saiba Mais</a>
+											<a class="btn-secondary" style="display:block" href="<?php echo $pais["link"];?>"><?php _e('Saiba Mais','egali'); ?></a>
 										</div>
 									</div>
 								</div>
