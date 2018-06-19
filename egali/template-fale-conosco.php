2b8996c3f4584213eb30cc0fef3d7f003c623be9
Giovane Zignani <giovane.zignani@conectt.com.br>
2018-06-19 18:42:36 -0300

    estrutura tradução fale conosco

--- egali/template-fale-conosco.php
+++ egali/template-fale-conosco.php
@@ -14,8 +14,8 @@ get_header();
 			<div class="row">
 				<nav aria-label="breadcrumb">
 					<ol class="breadcrumb">
-						<li class="breadcrumb-item"><a href="#">Home</a></li>
-						<li class="breadcrumb-item active" aria-current="page">FALE CONOSCO</li>
+						<li class="breadcrumb-item"><a href="#"><?php _e('Home','egali'); ?></a></li>
+						<li class="breadcrumb-item active" aria-current="page"><?php _e('FALE CONOSCO','egali'); ?></li>
 					</ol>
 				</nav>
 			</div>
@@ -25,10 +25,10 @@ get_header();
 		<div class="container position-relative">
 			<div class="row">
 				<div class="col-12 col-lg-7">
-					<h1>FALE CONOSCO</h1>
+					<h1><?php _e('FALE CONOSCO','egali'); ?></h1>
 					<div class="line-h-6 mb-5 mt-4"></div>
-					<P>Nossos consultores especializados estão esperando seu contato.</P>
-					<P>Preencha os campos e em até 24 horas você receberá retorno.</P>
+					<P><?php _e('Nossos consultores especializados estão esperando seu contato','egali'); ?>.</P>
+					<P><?php _e('Preencha os campos e em até 24 horas você receberá retorno','egali'); ?>.</P>
 
 					<?php echo do_shortcode( '[contact-form-7 id="69" title="Fale Conosco" html_class="mt-5"]' ); ?>
 
